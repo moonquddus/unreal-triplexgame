80cc40eca8ac551854ce7a50d69a37d2b4db73ea
Moon Quddus <moonquddus@gmail.com>
2020-12-28 22:51:26 +0000

    My very first function

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-int main(){
-    std::cout << "You're 133337 hacker who's trying to hack into KFC's servers to finally get a hold of the Colonel's secret recipe!";
-    std::cout << std::endl;
-    std::cout << "But first, you need to work out those finger-licking good codes...";
+void PrintIntroduction(){
+    std::cout << "You're 133337 hacker who's trying to hack into KFC's servers to finally get a hold of the Colonel's secret recipe!\n";
+    std::cout << "But first, you need to work out those finger-licking good codes...\n\n";
+}
 
+void PlayGame(){
     int CodeA = 4;
     int CodeB = 5;
     int CodeC = 6;
@@ -12,10 +13,10 @@ int main(){
     int CodeSum = CodeA + CodeB + CodeC;
     int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << std::endl;
-    std::cout << "- There are 3 digits in the code." << std::endl;
-    std::cout << "- The codes add up to: " << CodeSum << std::endl;
-    std::cout << "- The codes multiply to give: " << CodeProduct << std::endl;
+    PrintIntroduction();
+    std::cout << "- There are 3 digits in the code.";
+    std::cout << "\n- The codes add up to: " << CodeSum;
+    std::cout << "\n- The codes multiply to give: " << CodeProduct << std::endl;
 
     // I'm putting a comment here because the video tutorial told me to.
     /*
@@ -23,19 +24,20 @@ int main(){
     */
 
     int GuessA, GuessB, GuessC;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
     if (GuessSum == CodeSum && GuessProduct == CodeProduct){
-        std::cout << "You win!";
+        std::cout << "You win!\n";
     }
     else {
-        std::cout << "You lose :(";
+        std::cout << "You lose :(\n";
     }
+}
 
+int main(){
+    PlayGame();
     return 0;
 }
