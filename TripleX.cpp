a0440fba6567b7ba6d212452ebea4a618cc4ccd0
Moon Quddus <moonquddus@gmail.com>
2020-12-28 12:14:42 +0000

    additiooooooon

--- TripleX.cpp
+++ TripleX.cpp
@@ -6,9 +6,11 @@ int main(){
     std::cout << "But first, you need to work out those finger-licking good codes...";
 
     int a = 4;
+    int b = 5;
+    int c = 6;
 
     std::cout << std::endl;
-    std::cout << a;
+    std::cout << a + b + c;
 
 
     return 0;
