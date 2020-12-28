8ed75e28dc13b5e27273aa90892e962fadfae619
Moon Quddus <moonquddus@gmail.com>
2020-12-28 12:07:25 +0000

    My very first variable

--- TripleX.cpp
+++ TripleX.cpp
@@ -4,5 +4,12 @@ int main(){
     std::cout << "You're 133337 hacker who's trying to hack into KFC's servers to finally get a hold of the Colonel's secret recipe!";
     std::cout << std::endl;
     std::cout << "But first, you need to work out those finger-licking good codes...";
+
+    int a = 4;
+
+    std::cout << std::endl;
+    std::cout << a;
+
+
     return 0;
 }
