0f06ca2a2be4d6c267867dbe0e3caee4a51f8dd3
Moon Quddus <moonquddus@gmail.com>
2020-12-28 12:03:14 +0000

    Turned hello world into custom message

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello, World!";
+    std::cout << "You're 133337 hacker who's trying to hack into KFC's servers to finally get a hold of the Colonel's secret recipe!";
+    std::cout << std::endl;
+    std::cout << "But first, you need to work out those finger-licking good codes...";
     return 0;
 }
