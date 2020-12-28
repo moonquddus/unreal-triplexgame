202d6b42ee0e1d5000399044d2c873af15dc460a
Moon Quddus <moonquddus@gmail.com>
2020-12-28 12:20:21 +0000

    product variable + chaining system prints

--- TripleX.cpp
+++ TripleX.cpp
@@ -10,10 +10,11 @@ int main(){
     int c = 6;
 
     int sum = a + b + c;
+    int product = a * b * c;
 
     std::cout << std::endl;
-    std::cout << sum;
-
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
 
     return 0;
 }
