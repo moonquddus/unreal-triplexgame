2f7aa3bf3a771ac28611991218209f001df5ca25
Moon Quddus <moonquddus@gmail.com>
2020-12-29 13:37:09 +0000

    Finished the game!

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <ctime>
 
-void PrintIntroduction(){
-    std::cout << "You're 133337 hacker who's trying to hack into KFC's servers to finally get a hold of the Colonel's secret recipe!\n";
-    std::cout << "But first, you need to work out those finger-licking good codes...\n\n";
+void PrintIntroduction(int Difficulty){
+    std::cout << "\nYour new objective: work out the codes for level " << Difficulty;
+    std::cout << "...\n\n";
 }
 
-void PlayGame(){
-    int CodeA = 4;
-    int CodeB = 5;
-    int CodeC = 6;
+bool PlayGame(int Difficulty){
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
-    int CodeSum = CodeA + CodeB + CodeC;
-    int CodeProduct = CodeA * CodeB * CodeC;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
     std::cout << "- There are 3 digits in the code.";
     std::cout << "\n- The codes add up to: " << CodeSum;
     std::cout << "\n- The codes multiply to give: " << CodeProduct << std::endl;
@@ -26,18 +27,37 @@ void PlayGame(){
     int GuessA, GuessB, GuessC;
     std::cin >> GuessA >> GuessB >> GuessC;
 
-    int GuessSum = GuessA + GuessB + GuessC;
-    int GuessProduct = GuessA * GuessB * GuessC;
+    const int GuessSum = GuessA + GuessB + GuessC;
+    const int GuessProduct = GuessA * GuessB * GuessC;
 
     if (GuessSum == CodeSum && GuessProduct == CodeProduct){
-        std::cout << "You win!\n";
+        std::cout << "*** You've sussed it out! ***\n";
+        return true;
     }
     else {
-        std::cout << "You lose :(\n";
+        std::cout << "*** You guessed wrong :( ***\n";
+        return false;
     }
 }
 
 int main(){
-    PlayGame();
+    srand(time(NULL)); // create new random sequence based on time of day
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 5;
+
+    std::cout << "\n\nYou're 133337 hacker who's trying to hack into KFC's servers to finally get hold of the Colonel's secret recipe!\n";
+    std::cout << "But first, you need to work out those finger-licking good codes.\n\n";
+
+    while (LevelDifficulty <= MaxDifficulty){
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // clears errors
+        std::cin.ignore(); // discards the buffer (in case someone inputted stupid things)
+
+        if (bLevelComplete){
+            ++LevelDifficulty;
+        }
+    }
+
+    std::cout << "You found the secret recipe! The secret ingredient is MSG.\n";
     return 0;
 }
